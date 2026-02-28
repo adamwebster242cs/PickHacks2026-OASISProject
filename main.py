d2904d09fbad517b22f5c54fd93a2d4bd2588197
adamwebster242cs <adamwebster242@gmail.com>
2026-02-27 21:04:50 -0600

    Pushing first function skeletons
    We can add/remove more later.

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+import matplotlib.pyplot as plt
+
+#Lets make sure use a lot of comments on our code, so we can easily explain it if asked about it. We can also use these comments to help us remember what we were thinking when we wrote the code, and to help us debug it later on if we need to.
+
+def buildGrid(width, height):
+    return
+
+def getDistance(x1, y1, x2, y2):
+    return
+
+# If we use a function like this, we can cause whats called Urban Bloom, where we mark high cost areas initially, and the areas around it gradually decrease in cost. Causing an effect like a real cities' demand for something like this.
+def urbanBloom():
+    return
+
+def calculateTotalSystemCost():
+    return
+
+# I think we should use matplotlib to visualize our final "perfect" reservoir spot on a map using a heat map to show the demand across the city. We can use a color gradient to indicate areas of high demand (red) to low demand (blue), with the reservoir location marked clearly.
+def visualizeCity():
+    return
